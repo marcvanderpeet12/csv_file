630628cf93c551c5daed37bf47e65eb5bd3d675a
marcvanderpeet12 <marcvanderpeet@gmail.com>
2016-04-29 09:03:01 +0100

    - added changes

--- write_csv2.R
+++ write_csv2.R
@@ -3,6 +3,10 @@ setwd("~/csv_file/excel")
     x <- 12
     y <- 13
     df <- data.frame(x, y)
-    write.csv(df)
+    
+    z <- runif(1, 1, 10000)
+    z <- paste0(z, ".csv")
+    
+    write.csv(df, z)
 
 setwd("~/csv_file/")
