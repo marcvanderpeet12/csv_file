f41b0e3695107c920bc5487749fb244a7b2a2d63
marcvanderpeet12 <marcvanderpeet@gmail.com>
2016-04-29 08:55:23 +0100

    - added changes

--- write_csv2.R
+++ write_csv2.R
@@ -1,27 +1,8 @@
-x <- 12
-y <- 13
-df <- data.frame(x, y)
-write.csv(df)
+setwd("~/csv_file/excel")
 
-print("Hello")
+    x <- 12
+    y <- 13
+    df <- data.frame(x, y)
+    write.csv(df)
 
-print("nieuwe branch")
-
-read <- function(x,y){
-  
-  print(x*y)
-  
-}
-
-
-name <- function(name){
-  
-  print(name)
-
-}
-
-
-setwd("~/csv_file/source")
-source("multiply.R")
-
-z <- multiply(3,4)
+setwd("~/csv_file/")
